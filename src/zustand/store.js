506a28b06d5a29b001ae79da14bb46ed1eafbc90
ViacheslavBulakhov <95303344+ViacheslavBulakhov@users.noreply.github.com>
2024-01-22 17:46:37 +0200

    added setting for devtools

--- src/zustand/store.js
+++ src/zustand/store.js
@@ -4,31 +4,46 @@ import { ALL_COUNTRIES } from "../config";
 import { devtools } from "zustand/middleware";
 
 export const useStore = create(
-  devtools((set) => ({
-    countries: [],
-    filters: {
-      search: "",
-      region: null,
-    },
+  devtools(
+    (set) => ({
+      countries: [],
+      filters: {
+        search: "",
+        region: null,
+      },
 
-    setCountries: async () => {
-      const { data } = await axios.get(ALL_COUNTRIES);
-      set((state) => ({
-        ...state,
-        countries: [...data],
-      }));
-    },
+      setCountries: async () => {
+        const { data } = await axios.get(ALL_COUNTRIES);
+        set(
+          (state) => ({
+            ...state,
+            countries: [...data],
+          }),
+          false,
+          "setCountries"
+        );
+      },
 
-    setSearchFilter: (value) =>
-      set((state) => ({
-        ...state,
-        filters: { ...state.filters, search: value },
-      })),
+      setSearchFilter: (value) =>
+        set(
+          (state) => ({
+            ...state,
+            filters: { ...state.filters, search: value },
+          }),
+          false,
+          "setSearchFilter"
+        ),
 
-    setRegionFilter: (value) =>
-      set((state) => ({
-        ...state,
-        filters: { ...state.filters, region: value },
-      })),
-  }))
+      setRegionFilter: (value) =>
+        set(
+          (state) => ({
+            ...state,
+            filters: { ...state.filters, region: value },
+          }),
+          false,
+          "setRegionFilter"
+        ),
+    }),
+    { name: "Countries" }
+  )
 );
